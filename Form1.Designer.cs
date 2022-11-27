f9895c067a86c56ce8b3701fabec2abc2a0a49a8
RamazanDasci <115706826+RamazanDasci@users.noreply.github.com>
2022-11-27 18:52:33 +0300

    Add files via upload
    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Login_Ekranı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.yonetimpng = new System.Windows.Forms.ImageList(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.ogrenci = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.ImageIndex = 0;
+            this.button1.ImageList = this.yonetimpng;
+            this.button1.Location = new System.Drawing.Point(33, 61);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(244, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yönetim Bilgi\r\nSistemi Giriş";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yonetimpng
+            // 
+            this.yonetimpng.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("yonetimpng.ImageStream")));
+            this.yonetimpng.TransparentColor = System.Drawing.Color.Transparent;
+            this.yonetimpng.Images.SetKeyName(0, "stock-vector-toilet-restroom-wc-icon-set-in-thin-line-style-1226443066.png");
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.ImageIndex = 0;
+            this.button2.ImageList = this.ogrenci;
+            this.button2.Location = new System.Drawing.Point(327, 61);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(244, 105);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Öğrenci Bilgi \r\nSistemi Giriş";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // ogrenci
+            // 
+            this.ogrenci.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ogrenci.ImageStream")));
+            this.ogrenci.TransparentColor = System.Drawing.Color.Transparent;
+            this.ogrenci.Images.SetKeyName(0, "stock-vector-family-avatar-characters-vector-line-icons-2041700969.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(614, 231);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList yonetimpng;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList ogrenci;
+    }
+}
+
---
+++ yonetimbilgigiris.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Ekranı
+{
+    public partial class yonetimbilgigiris : Form
+    {
+        public yonetimbilgigiris()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = "Ramazan";
+            string sifre = "Ramazan46";
+
+            if(txt_id.Text == id && txt_sifre.Text == sifre)
+            {
+                yonetimpanel yonetimpanel = new yonetimpanel();
+                yonetimpanel.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adınız Veya Parolanız Hatalı", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void txt_sifre_TextChanged(object sender, EventArgs e)
+        {
+            txt_sifre.UseSystemPasswordChar = true;
+        }
+    }
+}
